802d77849f4cb2fdaa918622fbb6d0ab7aef4dd9
Sebastian Arboleda <SArboleda@arrivelogistics.com>
2018-04-02 18:58:31 -0500

    exercise

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
+  constructor() {
+    super();
+    this.increment = this.increment.bind(this);
+
+    this.state = { value: 0 }
   }
 
   render() {
     return (
       <div>
-        <span className="value">{this.state.count}</span>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })} id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.increment}>Incrementa</button>
       </div>
     );
+  }
+
+  increment() {
+    this.setState({ value: this.state.value + 1 });
   }
 }
 
